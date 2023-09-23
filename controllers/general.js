dc2d17b0a78c91cd525e764d819b6c99b8467249
philipthedeveloper <philipowolabi79@gmail.com>
2023-09-23 23:32:07 +0100

    Implement update change form email

--- controllers/general.js
+++ controllers/general.js
@@ -2,6 +2,7 @@ import { StatusCodes } from "http-status-codes";
 import { createBadRequestError } from "../errors/BadRequest.js";
 import emailSender from "../helpers/email-sender.js";
 import { Email } from "../models/index.js";
+import { createNotFoundError } from "../errors/NotFound.js";
 
 export const sendEmail = async (req, res) => {
   const text = req.body.heading || "New email for your business";
@@ -28,4 +29,19 @@ export const createEmail = async (req, res) => {
   return res
     .status(StatusCodes.CREATED)
     .json({ success: true, formEmail, message: "Email created" });
+};
+
+export const updateEmail = async (req, res) => {
+  const { email } = req.body;
+  const { emailId } = req.params;
+  if (!email) throw createBadRequestError("Please provide email");
+  const formEmail = await Email.findByIdAndUpdate(
+    emailId,
+    { email },
+    { new: true }
+  );
+  if (!formEmail) throw createNotFoundError("Email not found...");
+  return res
+    .status(StatusCodes.OK)
+    .json({ success: true, formEmail, message: "Updated Successfully." });
 };
--- router/general.js
+++ router/general.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { sendEmail, createEmail } from "../controllers/general.js";
+import { sendEmail, createEmail, updateEmail } from "../controllers/general.js";
 import validateToken from "../middlewares/validate-jwt.js";
 const generalRouter = express.Router();
 
 generalRouter.post("/send-email", sendEmail);
 generalRouter.post("/create-email", validateToken, createEmail);
+generalRouter.post("/update-email/:emailId", validateToken, updateEmail);
 
 export default generalRouter;
