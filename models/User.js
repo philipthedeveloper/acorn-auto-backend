4c89bb9b652c0f72373f2a0c7604cf35ee9b1e8c
philipthedeveloper <philipowolabi79@gmail.com>
2023-09-23 20:13:05 +0100

    Add timestamps to User Schema

--- models/User.js
+++ models/User.js
@@ -2,27 +2,30 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-const UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    trim: true,
-    required: [true, "Please provide email"],
-    unique: [true],
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Invalid email",
-    ],
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide email"],
+      unique: [true],
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Invalid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide password"],
+      trim: true,
+      minLength: [
+        8,
+        "The value of path `{PATH}` (`{VALUE}`) is shorter than the minimum allowed length ({MINLENGTH}).",
+      ],
+    },
   },
-  password: {
-    type: String,
-    required: [true, "Please provide password"],
-    trim: true,
-    minLength: [
-      8,
-      "The value of path `{PATH}` (`{VALUE}`) is shorter than the minimum allowed length ({MINLENGTH}).",
-    ],
-  },
-});
+  { timestamps: true }
+);
 
 // Setup bcryptjs for password hashing
 UserSchema.pre("save", async function () {
