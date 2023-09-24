e9979ad28b991edd6beeb3d220849ed11e60c811
philipthedeveloper <philipowolabi79@gmail.com>
2023-09-24 14:03:32 +0100

    Create controllers for handle CRUD operations for Holiday model

--- controllers/general.js
+++ controllers/general.js
@@ -1,7 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 import { createBadRequestError } from "../errors/BadRequest.js";
 import emailSender from "../helpers/email-sender.js";
-import { Email } from "../models/index.js";
+import { Email, Holiday } from "../models/index.js";
 import { createNotFoundError } from "../errors/NotFound.js";
 
 export const sendEmail = async (req, res) => {
@@ -48,4 +48,44 @@ export const updateEmail = async (req, res) => {
   return res
     .status(StatusCodes.OK)
     .json({ success: true, formEmail, message: "Updated Successfully." });
+};
+
+export const getHoliday = async (req, res) => {
+  let holiday;
+  const { type } = req.query;
+  if (type && type === "active") {
+    const today = new Date();
+    holiday = await Holiday.find({ holidayDate: { $gte: today } });
+  } else {
+    holiday = await Holiday.find({});
+  }
+  return res
+    .status(StatusCodes.OK)
+    .json({ success: true, holiday: holiday[0], nbHits: holiday.length });
+};
+
+export const createHoliday = async (req, res) => {
+  const { holidayText, holidayDate } = req.body;
+  if (!holidayText || !holidayDate)
+    throw createBadRequestError("Please provide holiday data");
+  const holiday = await Holiday.create(req.body);
+  return res
+    .status(StatusCodes.CREATED)
+    .json({ success: true, holiday, message: "Holiday created" });
+};
+
+export const updateHoliday = async (req, res) => {
+  const data = req.body;
+  const { holidayId } = req.params;
+  console.log(holidayId);
+  const numOfKeys = Object.keys(data).length;
+  if (numOfKeys === 0)
+    throw createBadRequestError("Please provide holiday data");
+  const holiday = await Holiday.findByIdAndUpdate(holidayId, data, {
+    new: true,
+  });
+  if (!holiday) throw createNotFoundError("Holiday not found...");
+  return res
+    .status(StatusCodes.OK)
+    .json({ success: true, holiday, message: "Update Successfully." });
 };
