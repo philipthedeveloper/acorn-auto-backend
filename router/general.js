57b32c15faeff28ce9fc75bec204f28af0ef01a4
philipthedeveloper <philipowolabi79@gmail.com>
2023-09-24 14:03:55 +0100

    map endpoints for holiday to controllers

--- router/general.js
+++ router/general.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { sendEmail, createEmail, updateEmail } from "../controllers/general.js";
+import {
+  sendEmail,
+  createEmail,
+  updateEmail,
+  createHoliday,
+  updateHoliday,
+  getHoliday,
+} from "../controllers/general.js";
 import validateToken from "../middlewares/validate-jwt.js";
 const generalRouter = express.Router();
 
 generalRouter.post("/send-email", sendEmail);
 generalRouter.post("/create-email", validateToken, createEmail);
 generalRouter.post("/update-email/:emailId", validateToken, updateEmail);
+generalRouter.get("/get-holiday", validateToken, getHoliday);
+generalRouter.post("/create-holiday", validateToken, createHoliday);
+generalRouter.post("/update-holiday/:holidayId", validateToken, updateHoliday);
 
 export default generalRouter;
